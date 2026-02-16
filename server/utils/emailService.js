4d6a1f9e115e09fe108ab83cd381ed0e7582e0c9
Ranjeet550 <91267236+Ranjeet550@users.noreply.github.com>
2026-02-16 18:19:29 +0530

    fix email service

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -3,12 +3,14 @@ const nodemailer = require('nodemailer');
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  port: parseInt(process.env.EMAIL_PORT),
   secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  logger: true,
+  debug: true
 });
 
 /**
